f858fa59c52600b9bbf1403d5d5359e30c9f1a90
Harry Chaplain <harry.chaplain@flutter.com>
2025-12-02 09:34:56 +0000

    task 2 part A

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,14 @@
 import tasks.Task01
+import tasks.Task02
 
 fun main() {
     println("TASK 1")
     println(Task01.partA())
     println(Task01.partB())
+    println("")
+
+    println("TASK 2")
+    println(Task02.partA())
+    println(Task02.partB())
     println("")
 }
---
+++ src/main/kotlin/tasks/Task02.kt
@@ -0,0 +1,30 @@
+package tasks
+
+import utils.readInput
+
+object Task02 : Task {
+
+    override fun partA(): Long = parseInput()
+        .flatMap { range ->
+            range.filter { id ->
+                val string = id.toString()
+                if (string.length % 2 != 0) false
+
+                val firstHalf = string.substring(0, string.length / 2)
+                val secondHalf = string.substring(string.length / 2)
+
+                firstHalf == secondHalf
+            }
+        }.sum()
+
+
+    override fun partB(): Int {
+        return 0
+    }
+
+    private fun parseInput() =
+        readInput("task02.txt").split(",").map { it ->
+            val parts = it.split("-")
+            parts.first().toLong()..parts.last().toLong()
+        }
+}
