bddb8807c90fd5cc5f200ef7ff26d2804b29bd4b
Harry Chaplain <harry.chaplain@flutter.com>
2025-12-03 15:46:37 +0000

    day 3 task 2

--- src/main/kotlin/tasks/Task03.kt
+++ src/main/kotlin/tasks/Task03.kt
@@ -8,22 +8,30 @@ object Task03 : Task {
         .sumOf { bank ->
             var maxJolt = 0
             bank.forEachIndexed { index, value ->
-               bank.forEachIndexed { otherIndex, otherValue ->
-                   if (index != otherIndex && index < otherIndex) {
-                       val jolt = "$value$otherValue".toInt()
-                       if (maxJolt < jolt) maxJolt = jolt
-                   }
-               }
+                bank.forEachIndexed { otherIndex, otherValue ->
+                    if (index != otherIndex && index < otherIndex) {
+                        val jolt = "$value$otherValue".toInt()
+                        if (maxJolt < jolt) maxJolt = jolt
+                    }
+                }
             }
             maxJolt
         }
 
 
-
-    override fun partB(): Long {
-        return 0
+    override fun partB(): Long = parseInputIndex().sumOf { bank ->
+        var usedIndex = -1
+        bank.windowed(bank.size - 11, 1).map { window ->
+            val highest = window.filterNot { usedIndex >= it.first }.sortedByDescending { it.second }.first()
+            usedIndex = highest.first
+            highest.second
+        }.joinToString(separator = "").toLong()
     }
 
     private fun parseInput() =
         readInput("task03.txt").split("\n").map { string -> string.map { it.digitToIntOrNull() } }
+
+    private fun parseInputIndex() =
+        readInput("task03.txt").split("\n")
+            .map { string -> string.mapIndexed { index, num -> index to num.digitToIntOrNull() } }
 }
